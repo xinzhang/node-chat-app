3a03c6d3741463ad45acaa26bec0e696b9286986
xin zhang <xin_zhang_2000@yahoo.com>
2017-08-30 22:43:12 +1000

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '/..', '/public');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-var server = app.listen(port);
-console.log('start to listening', port);
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', ()=> {
+        console.log('client disconnected');
+    })
+});
+
+
+server.listen(port, ()=> {
+    console.log('start to listening', port);
+});
+
+
